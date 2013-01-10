63dd428209baf9732164d319bbf5c990881a1912
Rowan Manning <accounts@rowanmanning.co.uk>
2013-01-10 23:18:50 +0000

    Handle dying worker processes

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ if (cluster.isMaster) {
         cluster.fork();
     }
 
+    // Listen for dying workers
+    cluster.on('exit', function (worker) {
+
+        // Replace the dead worker, we're not sentimental
+        console.log('Worker ' + worker.id + ' died :(');
+        cluster.fork();
+
+    });
+
 // Code to run if we're in a worker process
 } else {
 
