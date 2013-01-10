67f165f92da859dfe1dd40752401455d15aa50fb
Rowan Manning <accounts@rowanmanning.co.uk>
2013-01-10 22:27:43 +0000

    Add clustering

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-// Include Express
-var express = require('express');
+// Include the cluster module
+var cluster = require('cluster');
 
-// Create a new Express application
-var app = express();
+// Code to run if we're in the master process
+if (cluster.isMaster) {
 
-// Add a basic route – index page
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+    // Count the machine's CPUs
+    var cpuCount = require('os').cpus().length;
 
-// Bind to a port
-app.listen(3000);
-console.log('Application running!');
+    // Create a worker for each CPU
+    for (var i = 0; i < cpuCount; i += 1) {
+        cluster.fork();
+    }
+
+// Code to run if we're in a worker process
+} else {
+
+    // Include Express
+    var express = require('express');
+
+    // Create a new Express application
+    var app = express();
+
+    // Add a basic route – index page
+    app.get('/', function (req, res) {
+        res.send('Hello World!');
+    });
+
+    // Bind to a port
+    app.listen(3000);
+    console.log('Application running!');
+
+}
